c34ec453c87dd76b5751cecf8ecd6882a37f5dba
Marwabadr <elbadrmarwa20022gmail.com>
2024-02-10 06:26:27 -0800

    __init__

--- models/base_model.py
+++ models/base_model.py
@@ -11,18 +11,27 @@ class BaseModel:
     BaseModel class defines common attributes/methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
-    def __str__(s):
+    def __str__(self):
         """Return a string representation of the object"""
-        return "[{}] ({}) {}".format(s.__class__.__name__, s.id, s.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update attribute updated_at with the current datetime"""
+        """Update the attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
@@ -37,3 +46,16 @@ class BaseModel:
         result_dict['created_at'] = self.created_at.isoformat()
         result_dict['updated_at'] = self.updated_at.isoformat()
         return result_dict
+
+    def __eq__(self, other):
+        """
+        Compare two BaseModel instances for equality
+
+        Args:
+            other: Another BaseModel instance
+
+        Returns:
+            bool: True if the instances are equal, False otherwise
+        """
+        return isinstance(
+                other, BaseModel) and self.to_dict() == other.to_dict()
